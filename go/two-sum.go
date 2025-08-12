d621e044d7831992683cd248fa2438594af0b2ce
1-1is0 <amirftma@gmail.com>
2025-08-12 23:16:29 +0330

    simple two sum in go

---
+++ go/two-sum.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i, n1 := range nums {
+		for j, n2 := range nums[i+1:] {
+			// fmt.Println("i:", i, "j:", j, "n1:", n1, "n2:", n2)
+			if n1+n2 == target {
+				return []int{i, i + j + 1}
+			}
+		}
+		// fmt.Println("next i")
+	}
+	return []int{}
+}
+
+func main() {
+	// nums := []int{2, 7, 11, 15}
+	// target := 9
+	nums := []int{3, 2, 4}
+	target := 6
+	fmt.Println(twoSum(nums, target))
+}
