31a950363b00911be18c8ff7cc508ba221793f25
1-1is0 <amirftma@gmail.com>
2025-08-12 23:23:40 +0330

    two sum py hashmap

--- py/two-sum.py
+++ py/two-sum.py
@@ -10,10 +10,25 @@ class Solution:
                     return [i, j]
         return [0, 0]
 
+    def twoSumHashMap(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        # print(hashmap)
+        for i in range(0, len(nums)):
+            other_half_val = target - nums[i]
+            if other_half_val in hashmap and hashmap[other_half_val] != i:
+                return [i, hashmap[other_half_val]]
+        return [0, 0]
+
 
 
 
 nums = [2,7,11,15]
 target = 9
+
+# nums = [3, 2, 4]
+# target = 6
 sol = Solution()
-print(sol.twoSum(nums, target))
+# print(sol.twoSum(nums, target))
+print(sol.twoSumHashMap(nums, target))
