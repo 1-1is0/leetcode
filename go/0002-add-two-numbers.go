91792454f095e98616d38bc60d662741e4c7e6f9
1-1is0 <amirftma@gmail.com>
2025-08-15 01:17:12 +0330

    add two number from linked list in go

---
+++ go/0002-add-two-numbers.go
@@ -0,0 +1,41 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{Val: 0}
+	curr := head
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		val1 := 0
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+		val2 := 0
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+		res := val1 + val2 + carry
+		carry = res / 10
+		curr.Next = &ListNode{Val: res % 10}
+		curr = curr.Next
+	}
+	return head.Next
+}
+
+func main() {
+
+}
